15c45faa84018082252566ce174e1b1d711d2bd5
Erik Eldridge <erik@twitter.com>
2012-02-28 01:25:07 -0800

    Support bucket sizing

--- lib/rack/ab.rb
+++ lib/rack/ab.rb
@@ -1,81 +1,71 @@
-require 'rack'
-require 'digest/sha1'
-
 module Rack
-  
-  # The Rack::AB middleware splits users into buckets by selecting randomly from
-  # a collection of bucket names, setting this value in a cookie, and passing this
-  # value into the app via the env object.
+
+  # The Rack::AB middleware splits assigns a user to a bucket
+  # according the the user's id, and passes the bucket name
+  # into the app via the env object.
   #
   # Example
   #
-  # use Rack::AB, 
-  #   :cookie_name => 'new_cookie_name',
-  #   :bucket_names => [1,2,3]
+  # use Rack::AB,
+  #   :buckets => [{:v => 10}, {:b => 10}, {:n => 10}]
   #
-  # creates three buckets, 1, 2, and 3, randomly assigning each bucket name to a user, and
-  # storing the bucket name in a cookie called 'new_cookie_name', e.g., new_cookie_name=1
-  # 
+  # creates three buckets, 'v', 'b', and 'n'. Users will be assigned to buckets as follows:
+  # * ids in [0, 10) --> 'v'
+  # * ids in [10, 20) --> 'b'
+  # * ids in [20, 30) --> 'n'
+  # * ids in [30, 100) --> nil
+  #
   # Usage
   #
   # 1) Use Rack::AB middleware via 'use' directive
   # 2) Set options as desired
-  # 3) Check bucket value inside Rack app via the 'rack.ab.bucket_name' member of the 
+  # 3) Check bucket value inside Rack app via the 'rack.ab.bucket_name' member of the
   #    env object.
   # 4) Use this bucket name to split your traffic, eg if 'a' == env['rack.ab.bucket_name']:
   #    ...; elsif 'b' == env['rack.ab.bucket_name']: ... end
-  #    
-  
+  #
+
   class AB
-    
-    def self.release
-      "0.0.1"
-    end
-    
-    def initialize(app, options={})   
+
+    def initialize(app, options={})
       @app = app
-      
-      # Name of cookie for storing bucket name
-      @cookie_name = options[:cookie_name] || 'rack_ab'
-      
-      # Params to pass into Rack::Response::set_cookie
-      # http://rack.rubyforge.org/doc/classes/Rack/Response.src/M000179.html
-      @cookie_params = options[:cookie_params] || {}
-      
+
+      # Name of cookie for forcing bucket setting
+      @ab_cookie_name = options[:ab_cookie_name] || 'rack_ab'
+
+      # Name of cookie for setting the user id
+      @user_id_cookie_name = options[:user_id_cookie_name] || 'user_id'
+
+      # Bucket definitions
+      @buckets = options[:buckets] || [{:a => 50}, {:b => 50}]
+
     end
-    
+
     def call(env)
-      
+
       req = Request.new(env)
-      
-      # If user hasn't been assigned a bucket
-      if req.cookies[@cookie_name].nil?
-        
-        # TODO: determine uuid algorithm
-        bucket_name = Digest::SHA1.hexdigest Time.now.to_s
-      
-        # Add bucket name to env so app can split traffic internally
-        env["rack.ab.bucket_name"] = bucket_name
-        
-        status, headers, body = @app.call(env)
-        
-        resp = Rack::Response.new(body, status, headers)
-        
-        # Set cookie w/ bucket name
-        @cookie_params[:value] = bucket_name
-        resp.set_cookie(@cookie_name, @cookie_params)
-        resp.finish
-        
-      else
-        
-        env["rack.ab.bucket_name"] = req.cookies[@cookie_name]
-        
-        status, headers, body = @app.call(env)
-        
-        [status, headers, body]
+
+      if req.cookies[@ab_cookie_name]
+        env["rack.ab.bucket_name"] = req.cookies[@ab_cookie_name]
+      elsif req.cookies[@user_id_cookie_name]
+
+        user_id = req.cookies[@user_id_cookie_name].to_i
+
+        base_percentage = 0
+        @buckets.each do |bucket|
+
+          name, percentage = bucket.first
+
+          base_percentage += percentage
+          if user_id % 100 < base_percentage
+            env["rack.ab.bucket_name"] = name.to_s
+            break
+          end
+        end
+
       end
-      
+      @app.call(env)
     end
-    
+
   end
 end
--- test/spec_rack_ab.rb
+++ test/spec_rack_ab.rb
@@ -1,21 +1,140 @@
 require 'test/spec'
 require 'rack/mock'
-require 'rack/ab'
 
 context "Rack::AB" do
-  
-  specify "should expose release" do
-    Rack::AB.should.respond_to :release
-  end
-  
-  specify "should set bucket cookie if no cookie is set" do
+  specify "should provide a way to set a custom ab override cookie name" do
+
+    bucket_name = 'h'
+
     app = lambda { |env|
+
+      assert_equal env['rack.ab.bucket_name'], bucket_name
+
+      [200, {'Content-Type' => 'text/plain'}, '']
+    }
+
+    cookie_name = 'new_cookie_name'
+    app = Rack::AB.new(app, :ab_cookie_name => cookie_name)
+    Rack::MockRequest.new(app).get('/', 'HTTP_COOKIE' => "#{cookie_name}=#{bucket_name}")
+
+  end
+
+  specify "should provide a way to set a custom user id cookie name" do
+
+    app = lambda { |env|
+
+      assert_equal env['rack.ab.bucket_name'], 'a'
+
+      [200, {'Content-Type' => 'text/plain'}, '']
+    }
+
+    cookie_name = 'new_cookie_name'
+    app = Rack::AB.new(app, :user_id_cookie_name => cookie_name)
+    Rack::MockRequest.new(app).get('/', 'HTTP_COOKIE' => "#{cookie_name}=1")
+
+  end
+
+  specify "should provides a way to set a custom bucket sizes" do
+
+    app = lambda { |env|
+      assert_equal 'v', env['rack.ab.bucket_name']
+      [200, {'Content-Type' => 'text/plain'}, '']
+    }
+    app = Rack::AB.new(app, :buckets => [{:v => 10}, {:b => 10}, {:n => 10}])
+    Rack::MockRequest.new(app).get('/', 'HTTP_COOKIE' => 'user_id=5')
+
+    app = lambda { |env|
+      assert_equal 'b', env['rack.ab.bucket_name']
+      [200, {'Content-Type' => 'text/plain'}, '']
+    }
+    app = Rack::AB.new(app, :buckets => [{:v => 10}, {:b => 10}, {:n => 10}])
+    Rack::MockRequest.new(app).get('/', 'HTTP_COOKIE' => 'user_id=15')
+
+    app = lambda { |env|
+      assert_equal 'n', env['rack.ab.bucket_name']
+      [200, {'Content-Type' => 'text/plain'}, '']
+    }
+    app = Rack::AB.new(app, :buckets => [{:v => 10}, {:b => 10}, {:n => 10}])
+    Rack::MockRequest.new(app).get('/', 'HTTP_COOKIE' => 'user_id=25')
+
+    app = lambda { |env|
+      assert_equal nil, env['rack.ab.bucket_name']
+      [200, {'Content-Type' => 'text/plain'}, '']
+    }
+    app = Rack::AB.new(app, :buckets => [{:v => 10}, {:b => 10}, {:n => 10}])
+    Rack::MockRequest.new(app).get('/', 'HTTP_COOKIE' => 'user_id=35')
+  end
+
+  specify "should provide a way to split traffic inside app" do
+    app = lambda { |env|
+      if 'a' == env['rack.ab.bucket_name']
+        body = 'content for bucket a'
+      elsif 'b' == env['rack.ab.bucket_name']
+        body = 'content for bucket b'
+      end
+      [200, {'Content-Type' => 'text/plain'}, body]
+    }
+    app = Rack::AB.new(app)
+
+    response = Rack::MockRequest.new(app).get('/', 'HTTP_COOKIE' => 'user_id=164')
+
+    response.body.should.equal 'content for bucket b'
+  end
+
+end
+
+context "Rack::AB without user id or ab cookies" do
+  specify "should do nothing without ab or user id cookies" do
+    app = lambda { |env|
+
+      assert_nil env['rack.ab.bucket_name'], "should not set env"
+
+      [200, {'Content-Type' => 'text/plain'}, '']
+    }
+    Rack::AB.new(app).call({})
+  end
+end
+
+context "Rack::AB with user id in [0, 50)" do
+  specify "should put user in bucket 'a' by default" do
+    app = lambda { |env|
+
+      assert_equal env['rack.ab.bucket_name'], 'a'
+
       [200, {'Content-Type' => 'text/plain'}, '']
     }
     app = Rack::AB.new(app)
 
-    response = Rack::MockRequest.new(app).get('/', 'HTTP_COOKIE' => '')
-    response.headers['Set-Cookie'].should =~ /rack_ab=[a-zA-Z0-9]{32}/
-  end  
-  
+    Rack::MockRequest.new(app).get('/', 'HTTP_COOKIE' => 'user_id=13')
+  end
+end
+
+context "Rack::AB with user id in [50, 100)" do
+  specify "should be in bucket 'b' by default" do
+    app = lambda { |env|
+
+      assert_equal env['rack.ab.bucket_name'], 'b'
+
+      [200, {'Content-Type' => 'text/plain'}, '']
+    }
+    app = Rack::AB.new(app)
+
+    Rack::MockRequest.new(app).get('/', 'HTTP_COOKIE' => 'user_id=50')
+  end
+end
+
+context "Rack::AB with ab override cookie" do
+  specify "should put user in bucket specified by cookie" do
+
+    bucket_name = 'g'
+
+    app = lambda { |env|
+
+      assert_equal env['rack.ab.bucket_name'], bucket_name
+
+      [200, {'Content-Type' => 'text/plain'}, '']
+    }
+    app = Rack::AB.new(app)
+    Rack::MockRequest.new(app).get('/', 'HTTP_COOKIE' => "rack_ab=#{bucket_name}")
+  end
 end
